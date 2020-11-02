1d2ba308eb6dd458950b5fc91e8241abe9d7d500
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-02 11:49:58 +0330

    Add the SignUp component to the SignInAndSignUp page.

--- src/pages/SignInAndSignUp/SingInAndSignUp.tsx
+++ src/pages/SignInAndSignUp/SingInAndSignUp.tsx
@@ -1,11 +1,13 @@
 import "./SignInAndSignUp.scss";
 import React from "react";
 import SignIn from "../../components/SignIn/SignIn";
+import SignUp from "../../components/SignUp/SignUp";
 
 const SignInAndSignUp: React.FC = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
