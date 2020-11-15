81e4f7607f2727043f0e7d3c2b709b604a48140b
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-15 20:13:03 +0330

    Add the count of items added to the cart to cartIcon component.

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -3,23 +3,34 @@ import React, { FC } from "react";
 import { connect } from "react-redux";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { toggleCartHidden } from "../../Redux/actions/cartActions";
+import { IApplicationState } from "../../Redux/store/store";
 
 interface IProps {
   toggleCartHidden: typeof toggleCartHidden;
+  itemCount: number;
 }
 
-const CartIcon: FC<IProps> = ({ toggleCartHidden }) => {
+const CartIcon: FC<IProps> = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className="cart-icon" onClick={toggleCartHidden}>
       <ShoppingIcon className="shopping-icon" />
-      <span className="item-count">0</span>
+      <span className="item-count">{itemCount}</span>
     </div>
   );
 };
+
+const mapStateToProps = ({
+  cart: { cartItems },
+}: IApplicationState) => ({
+  itemCount: cartItems.reduce((acc, cartItem) => {
+    return acc + cartItem.quantity;
+  }, 0),
+});
+
 const mapDispatchToProps = (dispatch: any) => {
   return {
     toggleCartHidden: () => dispatch(toggleCartHidden()),
   };
 };
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
