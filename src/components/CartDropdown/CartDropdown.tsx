fe77ad7b4bb618abd7b1ea26228c8c8a71e82fa0
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-15 10:36:41 +0330

    Create CartItem component and add it to CartDropdown component. Also connect the cartDropdown to the store.cart.cartItems

--- src/components/CartDropdown/CartDropdown.tsx
+++ src/components/CartDropdown/CartDropdown.tsx
@@ -1,14 +1,33 @@
 import "./CartDropdown.scss";
 import React, { FC } from "react";
+import { connect } from "react-redux";
 import CustomButton from "../CustomButton/CustomButton";
+import { IApplicationState } from "../../Redux/store/store";
+import { ICartItem } from "../../Redux/types/cartTypes";
+import CartItem from "../CartItem/CartItem";
 
-const CartDropdown: FC = () => {
+interface IProps {
+  cartItems: ICartItem[];
+}
+
+const CartDropdown: FC<IProps> = ({ cartItems }) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        {cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
+const mapStateToProps = ({
+  cart: { cartItems },
+}: IApplicationState) => {
+  return {
+    cartItems,
+  };
+};
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/components/CartItem/CartItem.tsx
@@ -0,0 +1,25 @@
+import "./CartItem.scss";
+import React, { FC } from "react";
+import { ICartItem } from "../../Redux/types/cartTypes";
+
+interface IProps {
+  item: ICartItem;
+}
+
+const CartItem: FC<IProps> = ({
+  item: { imageUrl, price, name, quantity },
+}) => {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt={name} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
