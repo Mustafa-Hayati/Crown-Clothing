827d0884d0c05f90c34f9d75cc1af9970cb91110
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-16 18:26:33 +0330

    Change the IProps in cartDropdown to get access to the dispatch function that is provided to us by connect function. Dispatch toggleCartHidden when clicking on "GO TO CHECKOUT".

--- src/components/CartDropdown/CartDropdown.tsx
+++ src/components/CartDropdown/CartDropdown.tsx
@@ -1,19 +1,34 @@
 import "./CartDropdown.scss";
 import React, { FC } from "react";
+import { Dispatch } from "redux";
 import { connect } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router-dom";
+import { createStructuredSelector } from "reselect";
 import CustomButton from "../CustomButton/CustomButton";
 import { IApplicationState } from "../../Redux/store/store";
-import { ICartItem } from "../../Redux/types/cartTypes";
+import {
+  ICartItem,
+  ICartToggleHidden,
+} from "../../Redux/types/cartTypes";
 import CartItem from "../CartItem/CartItem";
 import { selectCartItems } from "../../Redux/selectors/cartSelectors";
-import { createStructuredSelector } from "reselect";
+import { toggleCartHidden } from "../../Redux/actions/cartActions";
 
 interface IProps extends RouteComponentProps {
   cartItems: ICartItem[];
+  dispatch: Dispatch<ICartToggleHidden>;
 }
 
-const CartDropdown: FC<IProps> = ({ cartItems, history }) => {
+const CartDropdown: FC<IProps> = ({
+  cartItems,
+  history,
+  /* NOTE
+   * We use dispatch this way to avoid writing
+   * mapDispatchToProps. Because the connect function gives
+   * us the access to dispatch function
+   */
+  dispatch,
+}) => {
   return (
     <div className="cart-dropdown">
       <div className="cart-items">
@@ -25,7 +40,12 @@ const CartDropdown: FC<IProps> = ({ cartItems, history }) => {
           <span className="empty-message">Your cart is empty</span>
         )}
       </div>
-      <CustomButton onClick={() => history.push("/checkout")}>
+      <CustomButton
+        onClick={() => {
+          history.push("/checkout");
+          dispatch(toggleCartHidden());
+        }}
+      >
         GO TO CHECKOUT
       </CustomButton>
     </div>
@@ -42,5 +62,10 @@ const mapStateToProps = createStructuredSelector<
 >({
   cartItems: selectCartItems,
 });
+
+/* NOTE
+ * The connect function passes the dispatch function to
+ * props, if we don't use mapDispatchToProps.
+ */
 
 export default withRouter(connect(mapStateToProps)(CartDropdown));
