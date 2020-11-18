f3240745b547f6ef1bab93d12600d33333edc022
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2020-11-18 05:55:49 +0330

    Remove redux-logger from production build.

--- src/Redux/store/store.ts
+++ src/Redux/store/store.ts
@@ -22,7 +22,11 @@ export interface IApplicationState {
   shop: IShopState;
 }
 
-const middlewares: Middleware[] = [logger];
+const middlewares: Middleware[] = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const store: Store<IApplicationState> = createStore(
   rootReducer,
